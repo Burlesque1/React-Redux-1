60ac23d193154d2a3efdaf01cc986d558e5870ab
Burlesque1 <123176000@qq.com>
2018-04-20 21:22:02 -0400

    controlled inputs for filters;

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,6 +7,7 @@ const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
     {props.expenses.map((expense) => {
+      console.log(expense, '+++', props);
       return <ExpenseListItem key={expense.id} {...expense} />;
     })}
   </div>
