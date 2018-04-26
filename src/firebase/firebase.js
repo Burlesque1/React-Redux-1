a8a558f81a259f961831dff983df6d24ff5ba3cf
Burlesque1 <123176000@qq.com>
2018-04-26 15:15:36 -0400

    promise firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,9 +8,34 @@ var config = {
   storageBucket: "expensify-60f61.appspot.com",
   messagingSenderId: "921601039893"
 };
-
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  name: 'Omelet'
+const database = firebase.database();
+
+database.ref().set({
+  name: 'gg',
+  age: 26,
+  isSingle: false,
+  location: {
+    city: 'Philadelphia',
+    country: 'United States'
+  }
+}).then(() => {
+  console.log('Data is saved!');
+}).catch((e) => {
+  console.log('This failed.', e);
+});
+
+// database.ref().set('This is my data.');
+
+// database.ref('age').set(27);
+// database.ref('location/city').set('New York');
+
+database.ref('attributes').set({
+  height: 73,
+  weight: 150
+}).then(() => {
+  console.log('Second set call worked.');
+}).catch((e) => {
+  console.log('Things didnt for the second error', e);
 });
