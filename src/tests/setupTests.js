a7823bfa5310b1d5f58b9a7307d19391e8fd3fa2
Burlesque1 <123176000@qq.com>
2018-05-02 18:36:32 -0400

    upgrade to react v16 and enzyme v3

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,10 +1,11 @@
-// import Enzyme from 'enzyme';
-// import Adaptor from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import DotEnv from 'dotenv';
 
-// Enzyme.configure({
-//     adapter: new Adapter()
-// });
+Enzyme.configure({
+    adapter: new Adapter()
+});
 
-
-require('dotenv').config({path: '.env.test'});
+// import DotEnv from 'dotenv'
+// DotEnv = require('dotenv')
+DotEnv.config({path: '.env.test'});
