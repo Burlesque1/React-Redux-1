bc161f922f6505c901f3bd3c268a29f24a6dae61
Burlesque1 <123176000@qq.com>
2018-05-02 19:58:44 -0400

    add hidden information

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,7 +5,7 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal, email }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal, email, hiddenAmount }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
   
@@ -13,7 +13,9 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal, email }) => {
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">Hi, {email} </h1>
-        <h5 className="page-header__title">Viewing <span>{expenseCount} </span>{expenseWord} totalling <span>{formattedExpensesTotal}</span> </h5>
+        <h5 className="page-header__title">Viewing <span>{expenseCount} </span> {expenseWord} totalling 
+          <span>{formattedExpensesTotal}</span>, and <span>{hiddenAmount}</span> more expenses hidden
+        </h5>
         <div className="page-header__actions">
           <Link className="button" to="/create"> Add Expense </Link>
         </div>
@@ -24,11 +26,13 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal, email }) => {
 
 const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  const hiddenAmount = state.expenses.length - visibleExpenses.length;
 
   return {
     expenseCount: visibleExpenses.length,
     expensesTotal: selectExpensesTotal(visibleExpenses),
-    email: state.auth.email
+    email: state.auth.email,
+    hiddenAmount
   };
 };
 
